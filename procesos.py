c37c0ef39f6878c25d8ec34bc0269c1da32c06b0
bvuelta <benat_vuelta@euneiz.com>
2025-12-17 09:04:11 +0100

    Update procesos.py

--- procesos.py
+++ procesos.py
@@ -1,25 +1,25 @@
-# modules/procesos.py
+# Importar el módulo os
 import os
-
+# Función para listar los procesos de ejecución
 def listar_procesos():
     procesos = []
     
     for pid in os.listdir('/proc'):
-        if pid.isdigit():
+        if pid.isdigit(): # Verificar si es número
             try:
-                with open(f'/proc/{pid}/status') as f:
+                with open(f'/proc/{pid}/status') as f: # Abrir el archivo (/proc/{pid}/status)
                     nombre = ""
-                    for linea in f:
+                    for linea in f: # Leer linea a linea
                         if linea.startswith("Name:"):
-                            nombre = linea.split()[1]
+                            nombre = linea.split()[1] # Si la línea comienza con "Name:" extraemos el nombre del proceso
                             break
 
                 procesos.append({
-                    'pid': int(pid),
-                    'name': nombre
+                    'pid': int(pid), # Convertir PID a num. entero
+                    'name': nombre # Nombre del proceso
                 })
 
-            except (FileNotFoundError, PermissionError):
+            except (FileNotFoundError, PermissionError): # Exception de error
                 continue
 
     return procesos
